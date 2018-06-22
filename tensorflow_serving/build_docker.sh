c24689089a9001f061878e24791ee7b628e7e82f
Marco Willi <will5448@umn.edu>
2018-06-22 00:03:44 -0500

    remove Dockerfile and streamline build_docker.sh

--- tensorflow_serving/build_docker.sh
+++ tensorflow_serving/build_docker.sh
@@ -1,17 +1,13 @@
 """ Build Tensorflow Serving Docker """
-cd ~/code
 git clone https://github.com/tensorflow/serving.git
 cd ./serving/tensorflow_serving/tools/docker/
-sudo docker build --pull -t $USER/tensorflow-serving-devel-cpu -f Dockerfile.devel .
+
+# OPTIONAL: Optimized build - Use Dockerfile.devel and adjust compile flags
+# bazel build -c opt --copt=-msse4.1 --copt=-msse4.2 tensorflow_serving/...
+# sudo docker build --pull -t $USER/tensorflow-serving-devel-cpu -f Dockerfile.devel .
+
+# Modify the Dockerfile to remove the entrypoint
+# add this CMD ["/bin/bash"] (like in the Dockerfile.devel)
+sudo docker build --pull -t $USER/tensorflow-serving-devel-cpu -f Dockerfile .
+
 sudo docker run -it -p 9000:9000 $USER/tensorflow-serving-devel-cpu /bin/bash
-
- cd tensorflow_serving/
- bazel build -c opt --copt=-msse4.1 --copt=-msse4.2 tensorflow_serving/...
-# INFO: Elapsed time: 7631.549s, Critical Path: 101.76s
-
-# verify if server is running (in /tensorflow-serving)
-bazel-bin/tensorflow_serving/model_servers/tensorflow_model_server
-
-# exit the container and commit changes
-# sudo docker ps (to get container id)
-sudo docker commit container_id $USER/tensorflow-serving-devel-cpu
