733d885a6d69d6577585499bae762410c825a56f
Marco Willi <will5448@umn.edu>
2018-06-21 19:49:14 -0500

    test image list

--- api/species/endpoints/client.py
+++ api/species/endpoints/client.py
@@ -24,18 +24,22 @@ class CameraTrapPrediction(Resource):
         try:
             req_data = request.get_json()
             print("got req_data: %s" % req_data)
-            image_url = req_data['url']
-            print("got url: %s" % image_url)
-            with req.urlopen(image_url) as url:
-                bytes = io.BytesIO(url.read())
-                image = bytes.getvalue()
+            image_url_list = req_data['url']
+            print("got url list: %s" % image_url_list)
+            image_byte_list = list()
+            for image_url in image_url_list:
+                with req.urlopen(image_url) as url:
+                    bytes = io.BytesIO(url.read())
+                    image = bytes.getvalue()
+                    image_byte_list.append(image)
 
         except Exception as inst:
             return {'message': 'something wrong with incoming request. ' +
                                'Original message: {}'.format(inst)}, 400
 
         try:
-            results = make_prediction(image)
+            results = make_prediction(image_byte_list)
+            print("Results Are %s: " % results)
             # results is: [(1, 0.9343334436416626), (0, 0.06039385870099068), (2, 0.005272684618830681)]
 
             results_json = [{'class': res[0], 'prob': res[1]} for res in results]
